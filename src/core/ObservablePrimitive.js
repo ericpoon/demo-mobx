571da9b1a036914c4610e2abf60770a4601ad598
Eric Zhiqi Pan <ericpoon510@gmail.com>
2018-05-15 14:59:59 +0800

    Code cleanup

--- src/core/ObservablePrimitive.js
+++ src/core/ObservablePrimitive.js
@@ -4,10 +4,6 @@ class ObservablePrimitive extends ObservableInterface {
   constructor(value, { name = '' } = {}) {
     super('[primitive] ' + name, false);
     this.value = value;
-    Object.defineProperty(this, 'autoruns', { enumerable: false });
-    Object.defineProperty(this, 'value', { enumerable: false });
-    Object.defineProperty(this, 'get', { enumerable: false });
-    Object.defineProperty(this, 'set', { enumerable: false });
   }
 
   get = () => {
