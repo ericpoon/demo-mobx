70d73f7fbe9205f21d39a40cd9739b81902452b6
Eric Zhiqi Pan <ericpoon510@gmail.com>
2018-05-18 13:20:40 +0800

    Add observer annotation for react components

---
+++ src/core/observer.js
@@ -0,0 +1,19 @@
+import autorun from './autorun';
+
+export default function observer(target) {
+  const render = target.prototype.render;
+  let autorunRegistered = false;
+  target.prototype.render = function () {
+    if (autorunRegistered) {
+      return render.apply(this, arguments);
+    } else {
+      let rendered = undefined;
+      autorun(() => {
+        rendered = render.apply(this, arguments);
+        this.forceUpdate(); // Official MobX also uses forceUpdate()
+      });
+      autorunRegistered = true;
+      return rendered;
+    }
+  };
+}
